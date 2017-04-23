e59900cf909b767702a85b4b69ab827455fb9a18
YBT <youngbrainstechnologies@gmail.com>
2017-04-23 21:34:55 +0530

    CR:Vineet

---
+++ app/assets/javascripts/bink.js
@@ -0,0 +1,3 @@
+var blink_speed = 500; var t = setInterval(function () { var ele = document.getElementById('blinker'); ele.style.visibility = (ele.style.visibility == 'hidden' ? '' : 'hidden'); }, blink_speed);
+
+Read more: http://html.com/tags/blink/#ixzz4ezA09n5y
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,30 @@
+$(function () {
+  var shrinkHeader = 800;
+  var slideImage = 700;
+  $(window).scroll(function () {
+    var scroll = getCurrentScroll();
+    if (scroll >= shrinkHeader) 
+      $('header').addClass('expand');
+    else 
+      $('header').removeClass('expand');
+    if (scroll >= slideImage) 
+      $('.paper-pen').addClass('slide');
+    else 
+      $('.paper-pen').removeClass('slide');
+  });
+  
+  function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+  }
+  i = 0;
+  list = ["Client Relationships", "Service Delivery", "Professional Consultant", "Support", "Job Opportunity"];
+  setInterval(function () {
+    change(i++)
+  }, 2000);
+
+  function change() {
+    if (i == list.length)
+      i = 0;
+    $('#text-change')[0].innerText = list[i];
+  }
+});
